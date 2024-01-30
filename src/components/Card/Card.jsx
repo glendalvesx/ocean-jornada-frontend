a6cd5622f40d2920ce05598ff1c134c6c68983ee
Glenda <107262765+glendalvesx@users.noreply.github.com>
2024-01-30 13:45:54 +0000

    Configuração da props para o Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,22 @@
-function Card() {
+function Card(props) {
+  //console.log(props)
+
+  const item = props.item
+
+  //console.log(item)
+
     return (
         <>
           <div className="card">
-            <h2>Rick Sanchez</h2>
+            <h2>{item.name}</h2>
 
             <div className="tags">
-            <p className="tag">Status: Vivo</p>
-            <p className="tag">Espécie: Humana</p>
-            <p className="tag">Origem: Terra C-137</p>
+               <p className="tag">Status: Vivo</p>
+               <p className="tag">Espécie: Humana</p>
+               <p className="tag">Origem: Terra C-137</p>
             </div>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+
+            <img src={item.image}/>
          </div>
         </>
        
