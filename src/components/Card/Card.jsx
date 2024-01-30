f552bb3dc90d6b3d5c6d4a7704c6c8de9569b34d
Glenda <107262765+glendalvesx@users.noreply.github.com>
2024-01-30 05:03:22 +0000

    Adicionado o elemento "tag"

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,10 @@ function Card() {
         <>
           <div className="card">
             <h2>Rick Sanchez</h2>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>    
+            <p>Status: Vivo</p>
+            <p>Espécie: Humana</p>
+            <p>Origem: Terra C-137</p>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
          </div>
         </>
        
