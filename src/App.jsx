7472502fe7f829b605033d809b0905a9134c6967
Glenda <107262765+glendalvesx@users.noreply.github.com>
2024-01-31 13:33:18 +0000

    Carregamento de dados via API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  //Carregamento de dados direto via código
   const item1 = {
     name: 'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
@@ -36,6 +37,30 @@ function App() {
   }
 
   const itens = [item1, item2, item3, item4]
+
+//Carregamento de dados via API (backend)
+async function carregarDadosApi(){
+  //Declarar a URL da API
+   const apiUrl = 'https://rickandmortyapi.com/api/character/'
+
+  //preparar e Executar a Requisição
+  const response = await fetch(apiUrl)
+
+  //console.log(response)
+
+  const body = await response.json()
+
+  console.log(body)
+
+  //Extrair a propriedade results do body
+  // Essa propriedade contém a lista de itens
+  const results = body.results;
+
+  }
+
+  // Chamando a função que carrega dados da API
+  carregarDadosApi()
+
 
   return (
     <>
